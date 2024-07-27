e918cc7e33c2be7af6b9a7ec9f4cfc6ee9043922
karinaromano <karinadevuk@gmail.com>
2024-07-27 15:32:15 -0300

    adding JS Plugin
    Adding JS Plugin typewriter

---
+++ JSPlugins/index.js
@@ -0,0 +1,11 @@
+//let heading = document.querySelector("h1");
+//heading.innerHTML = "Hello World";
+
+//typewriter js plugin: https://github.com/tameemsafi/typewriterjs
+
+new Typewriter("h1", {
+  strings: "Hello World, thank you for getting in touch", // in array ["Hello World"] to show and remove // "" to show and still
+  autoStart: true,
+  cursor: "", // to remove pipe
+  delay: 10, // faster
+});
